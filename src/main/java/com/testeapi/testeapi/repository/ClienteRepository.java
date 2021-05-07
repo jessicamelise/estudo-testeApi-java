392a5a6b0863bfe44e4e9c3bb18725594c6bd3f4
jessicamelise <jessica.melise@hotmail.com>
2021-05-07 19:55:03 -0300

    estudo com o spring boot

---
+++ src/main/java/com/testeapi/testeapi/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.testeapi.testeapi.repository;
+
+import com.testeapi.testeapi.model.Cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    
+}
